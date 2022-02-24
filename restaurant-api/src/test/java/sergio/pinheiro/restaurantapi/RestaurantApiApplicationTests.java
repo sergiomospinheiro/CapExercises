bda94b8a0202d5757aafbe8a4bbae7d52d06cff1
MOS PINHEIRO <sergio-miguel.mos-pinheiro@capgemini.com>
2022-02-24 12:16:32 +0000

    almost nothing

---
+++ restaurant-api/src/test/java/sergio/pinheiro/restaurantapi/RestaurantApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sergio.pinheiro.restaurantapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
